5bb47e535f2b1c13e37306e9a1c22214f7f3a2ad
Coyote <36854331+CoyoteSVB@users.noreply.github.com>
2026-05-28 13:23:48 +0200

    Add files via upload

---
+++ Azure_Storageaccount_Snapshot.ps1
@@ -0,0 +1,21 @@
+﻿$tenantId = ""
+$clientId = ""
+$clientSecret = ""
+$secureSecret = ConvertTo-SecureString $clientSecret -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($clientId, $secureSecret)
+$resourceGroup = "RG_EL_WE_01"
+$storageAccount = "ellispremiumprod"
+$fileShareName = "ftpdataprod"
+$cutoffDate = (Get-Date).AddDays(-$daysOld)
+ 
+Connect-AzAccount -ServicePrincipal -Tenant $tenantId -Credential $cred
+Set-AzContext -SubscriptionId "138c16cd-4619-4e52-a9c8-823391fc28f1"
+
+$snapshots = Get-AzRmStorageShare -ResourceGroupName $resourceGroup -StorageAccountName $storageAccount -IncludeSnapshot | Where-Object { $_.SnapshotTime -ne $null }
+
+foreach ($snapshot in $snapshots) {
+    Remove-AzRmStorageShare -ResourceGroupName $resourceGroup -StorageAccountName $storageAccount -Name $fileShareName -SnapshotTime $snapshot.SnapshotTime -Force
+}
+
+New-AzRmStorageShare -ResourceGroupName $resourceGroup -StorageAccountName $storageAccount -Name $fileShareName -Snapshot
+
